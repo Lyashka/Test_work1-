535ce420cb8630ecd88567ce8e868bee0d7248c6
ILA <merzlyaha@mail.ru>
2022-05-01 20:00:56 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+
+"use sctrict";
+
+
+
+console.log("GG");
+
+let number = 5;
+
+number = 10;
+console.log(number);
+
+{
+    let F = 21;
+    console.log("Мне " + F);
+
+}
+
+let obj = {
+    name:"jhon",
+    age: 21,
+};
+console.log(obj.name);
+
